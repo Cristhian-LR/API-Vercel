12468172973c2bddb8387781ad835a5d19b6076b
cristhian <15186@ulsachihuahua.edu.mx>
2025-11-26 23:01:01 -0600

    veriosn 2 API

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = 3000;
 
-// RUTA /
-app.get("/", (req, res) => {
-  res.send("Integrantes: Cristhian, Sebastián");
+// Ruta 1
+app.get('/', (req, res) => {
+  res.send('Bienvenidos al API');
 });
 
-// RUTA /ulsa
-app.get("/ulsa", (req, res) => {
-  const mision = "La misión de la ULSA Chihuahua es formar integralmente a personas inspiradas en el carisma lasallista y la fe cristiana, para que contribuyan a una sociedad más justa, solidaria y sostenible. Esto lo logra a través de su educación profesional, el desarrollo de competencias, el fomento de valores, la inserción laboral y el cuidado de la Casa Común.";
-  res.send(mision);
+// Ruta 2
+app.get('/mensaje1', (req, res) => {
+  res.send('Aquí va el mensaje 1. ¡Hola desde Express!');
 });
 
-// RUTA /saludar/:nombre
-app.get("/saludar/:nombre", (req, res) => {
-  const { nombre } = req.params;
-  res.send(`Hola ${nombre}`);
+// Ruta 2
+app.get('/ulsa', (req, res) => {
+  res.send('La misión de la ULSA Chihuahua es formar integralmente a personas inspiradas en el carisma lasallista y la fe cristiana, para que contribuyan a una sociedad más justa, solidaria y sostenible. Esto lo logra a través de su educación profesional, el desarrollo de competencias, el fomento de valores, la inserción laboral y el cuidado de la Casa Común. ');
+});
+// Ruta 3
+app.get('/saludar/:msg', (req, res) => {
+  res.send(`Hola: ${req.params.msg} ߘ`);
 });
 
-// Para desarrollo local
-if (require.main === module) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log("Servidor local en puerto " + port));
-}
-
-// Para Vercel
-module.exports = (req, res) => {
-  app(req, res);
-};
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
